5aef971a607f14daa25a1179c13c9d6f14f408d0
Rahul <rahulna@thoughtworks.com>
2016-02-17 16:05:43 +0530

    added some instructions in test.js, solved spelling mistake of variables in test.js

--- testing/test.js
+++ testing/test.js
@@ -2,8 +2,13 @@ var child_process = require('child_process');
 var exec = child_process.exec;
 var child = child_process.child;
 
-// junit renamed to junit.jar
-//hamcrest renamed to hamcrest.jar
+// rename junit-4.xx.jar to junit.jar
+//rename hamcrest-core.jar to hamcrest.jar
+
+//run test command : node test.js MyClass.java
+//test file name shoud be MyClassTest.java
+
+//All file shoud be kept in same directory
 
 var showErr = function(error){
     if(error.code == 'ENOENT'){
@@ -11,6 +16,7 @@ var showErr = function(error){
         console.log('-->Error: '+message);
     }
 }
+
 var showOutput = function (error, stdout, stderr){
     console.log(stdout);
     if(stderr!= '')
@@ -20,18 +26,21 @@ var showOutput = function (error, stdout, stderr){
 }
 var compile_and_run = function(){
     var arguements = process.argv.slice(2);
+
     var main = arguements[0] || 'Main.java';
-    var Class = arguements[0].split('.')[0];
+    var Class = main.split('.')[0];
     var test = Class+'Test.java';
     var executable = Class+'Test';
 
     var compileMain = 'javac '+main;
-    var complileTest = 'javac -cp .:junit.jar ' + test;
+    var compileTest = 'javac -cp .:junit.jar ' + test;
     var runTest = 'java -cp .:junit.jar:hamcrest.jar org.junit.runner.JUnitCore ' + executable;
-    var command = compileMain+' && ' + complileTest + ' && ' + runTest;
+
+    var command = compileMain+' && ' + compileTest + ' && ' + runTest;
+
     console.log('Executing the following command:');
     console.log(compileMain);
-    console.log(complileTest);
+    console.log(compileTest);
     console.log(runTest);
     console.log('-----------------------------------------------------------------------------')
 
