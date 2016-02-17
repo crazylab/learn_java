9a0d2ee0e0fc01c25867b97cf387eac2094a9688
Rahul <rahulna@thoughtworks.com>
2016-02-17 10:06:14 +0530

    Added menu option to perform a particular task

--- String/Main.java
+++ String/Main.java
@@ -2,13 +2,38 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main{
+    static void showOptions(){
+        String[] options = {
+            "Count Vowels",
+            "Count Consonants"
+        };
+        System.out.println();
+        for(int index = 0; index < options.length; index++){
+            System.out.println((index+1) + ". " + options[index]);
+        }
+    }
     public static void main(String[] args) {
         Console console = System.console();
-        console.printf("Enter A String: ");
-        String input = console.readLine();
-
         String_Method sMethod = new String_Method();
-        sMethod.print(input + " has " + sMethod.countVowels(input) + " Vowels");
-        sMethod.print(input + " has " + sMethod.countConsonants(input) + " Consonants");
+        boolean repeat;
+        do{
+            console.printf("Enter A String: ");
+            String input_string = console.readLine();
+            showOptions();
+            console.printf("--> Select an operation from above to perform: ");
+            String operation = console.readLine();
+
+            switch(Integer.parseInt(operation)){
+                case 1 :    sMethod.print("\n" + input_string + " has " + sMethod.countVowels(input_string) + " Vowels");
+                            break;
+                case 2 :    sMethod.print("\n" + input_string + " has " + sMethod.countConsonants(input_string) + " Consonants");
+                            break;
+                default:    sMethod.print("\nPlease select a valid option");
+            }
+
+            console.printf("\nWant to continue ?(y/n) ");
+            input_string = console.readLine();
+            repeat = input_string.charAt(0) == 'y';
+        }while(repeat);
     }
 }
