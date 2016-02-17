6cfd616161f34d4c84caeddbc1f466b8f0d06449
Rahul <rahulna@thoughtworks.com>
2016-02-17 16:34:08 +0530

    Can not be used multiple child process due to sync problm. Single child process executing

--- testing/test.js
+++ testing/test.js
@@ -5,7 +5,7 @@ var child = child_process.child;
 // rename junit-4.xx.jar to junit.jar
 //rename hamcrest-core.jar to hamcrest.jar
 
-//run test command : node test.js MyClass.java
+//run test command : node test.js MyClass
 //test file name shoud be MyClassTest.java
 
 //All file shoud be kept in same directory
@@ -27,22 +27,21 @@ var showOutput = function (error, stdout, stderr){
 var compile_and_run = function(){
     var arguements = process.argv.slice(2);
 
-    var main = arguements[0] || 'Main.java';
+    var main = arguements[0] || 'Main';
     var Class = main.split('.')[0];
-    var test = Class+'Test.java';
-    var executable = Class+'Test';
+    var test = Class + 'Test.java';
+    var executable = Class + 'Test';
 
-    var compileMain = 'javac '+main;
+    var compileMain = 'javac ' + main + '.java';
     var compileTest = 'javac -cp .:junit.jar ' + test;
     var runTest = 'java -cp .:junit.jar:hamcrest.jar org.junit.runner.JUnitCore ' + executable;
+    var command = compileMain + ' && ' + compileTest + ' && ' + runTest;
+    exec(command, showOutput);
 
     console.log('Executing the following command:');
     console.log(compileMain);
-    child = exec(compileMain, showOutput);
     console.log(compileTest);
-    exec(compileTest, showOutput);
     console.log(runTest);
-    exec(runTest, showOutput);
     console.log('-----------------------------------------------------------------------------');
 
 }
